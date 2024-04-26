da3822123018029a78949e09b82c430e72799d31
aaqib404 <aaqib.khan@dariel.co.za>
2024-04-26 08:25:35 +0200

    add interface

---
+++ src/app/models/country.ts
@@ -0,0 +1,15 @@
+export interface Country {
+    name: string;
+    population: number;
+    region: string;
+    subregion: string;
+    languages: string[];
+    borders: string[];
+    capital: string;
+    flag: string;
+    currencies: string[];
+    timezones: string[];
+    area: number;
+    government: string;
+    continents: string[];
+}
